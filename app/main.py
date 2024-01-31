5a38c07d4fa7be40d83f998d32e60982ddd3fd92
jcvillamizarh <jcvillamizarh@gmail.com>
2024-01-31 10:41:50 -0500

    Se agrega libreria pandas y se hace lectura de archivo csv pro medio de la libreria, y se actualiza archivo de requirements

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
+'''
 def draw_country_population_bar():
   data = read_csv.read_csv('./data.csv')
   country = input('Type Country to get data => ')
@@ -17,11 +19,30 @@ def draw_world_population_percetages_pie():
   data = list(filter(lambda item: item['Continent'] == 'South America', data))
   countries, percentages = utils.world_population_percentage(data)
   charts.generate_pie_chart(countries, percentages)
+'''
+
+def draw_pie_char():
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie_chart(countries, percentages)
+
+  data = read_csv.read_csv('./data.csv')
+  country = input('Type Country to get data => ')
+  result = utils.population_by_country(data, country)
+  if len(result) > 0:
+    country = result[0]
+    labels, values = utils.get_population(country)
+    charts.generate_bar_chart(country['Country/Territory'], labels, values)
 
 
 def run():
+  draw_pie_char()
+  '''
   draw_world_population_percetages_pie()
   draw_country_population_bar()
+  '''
 
 if __name__ == '__main__':
   run()
